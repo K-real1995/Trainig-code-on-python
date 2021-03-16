1935ff0544ba508ba68bf97232045f86984348ae
K-real1995 <77657168+K-real1995@users.noreply.github.com>
2021-03-16 16:11:58 +0700

    Update 04_my_family.py

--- Skillbox/Lesson_2/04_my_family.py
+++ Skillbox/Lesson_2/04_my_family.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#training with lists
 
-# Создайте списки:
 
-# моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
 my_family = ["son", "daughter" "father", "grand father", "mother", "grand mother"]
 
 
-# список списков приблизителного роста членов вашей семьи
+
 my_family_height = [["son",158],["daughter",149],["father",186],["grand father",185], ["mother",175],
                 ["grand mother", 165]]
 
 
-# Выведите на консоль рост отца в формате
-#   Рост отца - ХХ см
+
 print(my_family_height[3][1], "см")
 
-# Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
-#   Общий рост моей семьи - ХХ см
 height_son = my_family_height[0][1]
 height_daughter=my_family_height[1][1]
 height_father=my_family_height[2][1]
