82e23fb5005bcb8fee062bd3d528102e55a9192a
K-real1995 <garfild17@yandex.ru>
2021-06-10 19:33:10 +0700

    all tasks completed

--- working_with_tuples.py
+++ working_with_tuples.py
@@ -5,3 +5,12 @@
 n=int(input("Введите длину предполагаемого кортежа (не включительно):"))
 my_tuple = tuple(i for i in range(1, n))
 print(my_tuple)
+
+my_list=[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+print(my_list[1::2])
+
+l = [int(i) for i in input().split()]
+def modify_list(l):
+    l[:] = [i // 2 for i in l if i % 2 == 0]
+print(modify_list(l))
+print(l)
