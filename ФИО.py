2687563ceb22c4884b94bd7fec3d897487a85c72
K-real1995 <77657168+K-real1995@users.noreply.github.com>
2022-02-27 13:53:29 +0700

    Delete ФИО.py

--- ФИО.py
+++
@@ -1,8 +0,0 @@
-import sys
-first_name = sys.argv[1]
-last_name = sys.argv[2]
-second_name = sys.argv[3]
-first_name = first_name.capitalize()
-last_name = last_name.capitalize()
-second_name = second_name.capitalize()
-print(first_name + " " + last_name + " " + second_name)
