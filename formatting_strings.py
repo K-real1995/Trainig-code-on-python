b8e2dbdf1c8aba7d5546f041400c5d716b2cc3bf
K-real1995 <77657168+K-real1995@users.noreply.github.com>
2022-02-27 13:56:17 +0700

    Delete formatting_strings.py

--- formatting_strings.py
+++
@@ -1,8 +0,0 @@
-age = 18
-street = 'Ленина'
-first_name = 'Олег'
-house = 58
-city_r = 'Москве'
-d = {"name":first_name , "age":age, "city":city_r, "street":street, "house":house}
-hello = 'Привет! Меня зовут %(name)s, мне %(age)d лет. Я живу в %(city)s, на улице %(street)s, дом %(house)d.' % d
-print(hello)
